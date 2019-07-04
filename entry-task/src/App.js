825ccd03551199ce57e32496a9405f79c04c9e06
Kezia Suhendra <keziasuhendra44@gmail.com>
2019-07-04 23:02:10 +0700

    add page header and hero-shot

--- entry-task/src/App.js
+++ entry-task/src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../public/y-logo-white.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="hero-shot">
+        <div className="layer">
+          <div>
+            <img className="logo" src={logo}/>
+          </div>
+          <h1 className="hello">Hello! I'm Kezia Suhendra</h1>
+          <h2 className="consult">Consult, Design, and Develop Websites</h2>
+          <h4 className="shot">
+            Have something great in mind? Feel free to contact me. <br/>
+            I'll help you to make it happen.
+          </h4>
+          <div className="button button-contact">LET'S MAKE CONTACT</div>
+          <div className="bottom-margin"/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
